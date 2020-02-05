6f0d9166b74412380e1318ec37e7765640f01ebb
BluDemons <jar.salazar@yavirac.edu.ec>
2020-02-05 09:48:56 -0500

    mejora view login e iconos

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -12,23 +12,25 @@ const Home = () => {
             <main className="my-8">
                 <hr />
                 <div className="flex mb-4 h-56">
-                    <div className="w-1/4 h-48 flex-1 text-center px-4 py-2 m-2">
-                        <div className=" rounded  rounded-t-lg bg-pink-300 overflow-hidden shadow max-w-xs my-2 pt-20">
-                            <div className="flex justify-center -mt-8">
-                                <img src="http://www.cavsi.com/preguntasrespuestas/images/que-es-usuario.jpg" className="rounded-full h-24 w-24 border-solid border-white border-2 -mt-5" />		
-                            </div>
-                            <div className="text-center px-3 pb-6 pt-2 bg-white pt-5 hover:bg-gray-400 ">
-                                <h3 className="text-black text-sm bold uppercase font-sans">Administrador</h3>
+                <div className="w-1/3 h-48 flex-1 text-center px-4 py-2">
+                        <div className="max-w-xs rounded overflow-hidden shadow-lg my-2">
+                            <img className="w-full" src="https://lh4.googleusercontent.com/proxy/gcPI9_7KcNOYzKPcfLVwa1a6WMufmoMKD7ST7aeZDsms9NdLRVnFm7_BXL-pyUmZ-XtS1kPUC7A52vRqUrRhokRe1NzUflGxmnN4kq8Y4WqZvskAgvcoiDBrpe8" alt="Sunset in the mountains"></img>
+                            <div className="px-6 py-4">
+                            <div className="font-bold text-xl mb-2">Administrador</div>
+                                <p className="text-grey-darker text-base">
+                                    2
+                                </p>
                             </div>
                         </div>
                     </div>
-                    <div className="w-1/4 h-48 flex-1 text-center px-4 py-2 m-2">
-                        <div className=" rounded  rounded-t-lg bg-pink-300 overflow-hidden shadow max-w-xs my-2 pt-20">
-                            <div className="flex justify-center -mt-8">
-                                <img src="https://image.flaticon.com/icons/png/512/44/44948.png" className="rounded-full h-24 w-24 border-solid bg-white border-white border-2 -mt-5" />		
-                            </div>
-                            <div className="text-center px-3 pb-6 pt-2 bg-white pt-5 hover:bg-gray-400 ">
-                                <h3 className="text-black text-sm bold uppercase font-sans">Estudiantes</h3>
+                    <div className="w-1/3 h-48 flex-1 text-center px-4 py-2">
+                        <div className="max-w-xs rounded overflow-hidden shadow-lg my-2">
+                            <img className="w-full" src="https://lh4.googleusercontent.com/proxy/gcPI9_7KcNOYzKPcfLVwa1a6WMufmoMKD7ST7aeZDsms9NdLRVnFm7_BXL-pyUmZ-XtS1kPUC7A52vRqUrRhokRe1NzUflGxmnN4kq8Y4WqZvskAgvcoiDBrpe8" alt="Sunset in the mountains"></img>
+                            <div className="px-6 py-4">
+                            <div className="font-bold text-xl mb-2">Administrador</div>
+                                <p className="text-grey-darker text-base">
+                                    2
+                                </p>
                             </div>
                         </div>
                     </div>
--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -38,51 +38,70 @@ class Login extends Component {
   render() {
     const { email, password } = this.state
     return (
-      <div className="font-mono">
-        <div className="container mx-auto">
-          <div className="flex justify-center px-6 my-12">
-            <div className="w-full xl:w-3/4 lg:w-11/12 flex">
-              <div className="w-full h-auto bg-gray-400 hidden lg:block lg:w-1/2 bg-cover rounded-l-lg">
-
-              </div>
-              <div className="w-full lg:w-1/2 bg-white p-5 rounded-lg lg:rounded-l-none">
-                <h3 className="pt-4 text-2xl text-center">Bienvenido!</h3>
-                <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded">
-                  <div className="mb-4">
-                    <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="persona_email">
-                      Correo Institucional
-                  </label>
-                    <input className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                      type="text"
-                      placeholder="correo@yavirac.edu.ec"
-                      name="email"
-                      value={email}
-                      onChange={ this.changeHandler } 
-                    />
-                  </div>
-                  <div className="mb-4">
-                    <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="persona_clave">
-                      Contraseña
-                  </label>
-                    <input className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                      type="password"
-                      name="password"
-                      value={ password }
-                      onChange={ this.changeHandler } 
-                    />
-                  </div>
-                  <div className="mb-6 text-center">
-                      <button type="submit"  className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline">
-                        Ingresar
-                    </button>
-                  </div>
-                  <hr className="mb-6 border-t" />
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+		<div className="mx-auto">
+			<div className="flex justify-center px-6 my-12">
+				<div className="w-full xl:w-3/4 lg:w-11/12 flex">					
+					<div
+						className="bg-fixed w-full h-auto bg-gray-400 hidden lg:block lg:w-1/2 bg-cover rounded-l-lg"
+						style={{backgroundImage: `url('https://cdn.cp.adobe.io/content/2/rendition/ea8d25f5-eefd-4da7-a337-1cfa6e7e5e02/artwork/91402499-4385-424e-90ee-a647d0915cee/version/0/format/jpg/dimension/width/size/300')`}}></div>				
+					<div className="w-full lg:w-1/2 bg-gray-800 p-5 rounded-lg lg:rounded-l-none">
+						<h3 className="pt-4 text-2xl text-white text-center">Bienvenido de Vuelta!!</h3>
+						<form className="px-8 pt-6 pb-8 mb-4 bg-gray-800 rounded">
+							<div className="mb-4 bg-gray-800">
+								<label className="block mb-2 text-sm font-bold text-gray-600" htmlFor="username">
+									Usuario
+								</label>
+								<input
+									className="w-full px-3 py-2 bg-gray-800 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+									type="text"
+                  placeholder="correo@yavirac.edu.ec"
+                  name="email"
+                  value={email}
+                  onChange={ this.changeHandler } 
+								/>
+							</div>
+							<div className="mb-4">
+								<label className="block mb-2 text-sm font-bold text-gray-600" htmlFor="password">
+									Contraseña
+								</label>
+								<input
+									className="w-full px-3 py-2 bg-gray-800 mb-3 text-sm leading-tight text-gray-700 border border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+									type="password"
+                  placeholder="******************"
+                  name="password"
+                  value={ password }
+                  onChange={ this.changeHandler } 
+								/>								
+							</div>
+							{/* <div class="mb-4">
+								<input class="mr-2 leading-tight" type="checkbox" id="checkbox_id" />
+								<label class="text-sm" for="checkbox_id">
+									Remember Me
+								</label>
+							</div> */}
+							<div className="mb-6 text-center">
+								<button
+									className="w-full px-4 py-2 font-bold text-gray-800 bg-blue-600 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+									type="button"
+								>
+									Ingresar
+								</button>
+							</div>
+							<hr className="mb-6 border-t" />							
+							<div class="text-center text-gray-500">
+								Copyright &copy; <a
+									class="inline-block text-sm text-blue-500 align-baseline hover:text-white"
+									href="./home"
+								>
+									 THWS
+								</a>&nbsp;
+                2020
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
     );
   }
 }
