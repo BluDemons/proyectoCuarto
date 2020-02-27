32b9a1ee4966018a0a5df1ceade0c9dcfd500bf9
Roger Cedeño <grarogccee@gamil.com>
2020-02-26 19:10:05 -0500

    geo

--- frontend/react_movile/App.js
+++ frontend/react_movile/App.js
@@ -1,8 +1,27 @@
-import React, { Component } from 'react';
-import { AppRegistry, View } from 'react-native';
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  TouchableOpacity
+} from "react-native";
 import Route from './routes';
 
 class App extends Component {
+
+   componentDidMount = () => {
+      navigator.geolocation.watchPosition(
+        posicion => {
+          const coordenadas = JSON.stringify(posicion);
+          this.setState({ coordenadas });
+        },
+        error => Alert.alert(error.message),
+        { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+      );
+    };
+
    render() {
       return (
          <Route />
